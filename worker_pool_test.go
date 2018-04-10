f749bdce0f6f3365e54ea51dcb23c2dc51349463
Gui B.L <guilhebl@gmail.com>
2018-04-10 01:32:03 -0700

    improving code and adding new tests

--- worker_pool_test.go
+++ worker_pool_test.go
@@ -3,6 +3,9 @@ package job
 import (
 	"strconv"
 	"testing"
+	"strings"
+	"unicode"
+	"math/rand"
 )
 
 // Anonymous Task
@@ -27,6 +30,55 @@ func (e *TestSumTask) Run(payload Payload) JobResult {
 
 func NewTestSumTask() TestSumTask {
 	return TestSumTask{}
+}
+
+// Creates a sample sum Job task which aims to sum X and Y as int
+func NewSumJobTask(x, y int) Job {
+	ret := NewJobResultChannel()
+	m := make(map[string]string)
+	m["x"] = strconv.Itoa(x)
+	m["y"] = strconv.Itoa(y)
+	task := NewTestSumTask()
+	work := NewJob(&task, m, ret)
+	return work
+}
+
+// Creates a sample task that sums all the Rune int values of a string
+func NewSumRunesOfWordTask() SumRuneTask {
+	return SumRuneTask{}
+}
+
+func NewSumRunesOfWordJobTask(word string) Job {
+	ret := NewJobResultChannel()
+	m := make(map[string]string)
+	m["word"] = word
+	task := NewSumRunesOfWordTask()
+	work := NewJob(&task, m, ret)
+	return work
+}
+
+// Anonymous SumRune Task
+type SumRuneTask struct{}
+
+func spaceMap(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, str)
+}
+
+func (e *SumRuneTask) Run(payload Payload) JobResult {
+	word := payload.Params["word"]
+	word = spaceMap(word)
+
+	res := int64(0)
+	for _, r := range word {
+		num := int64(r)
+		res += num
+	}
+	return NewJobResult(res, nil)
 }
 
 func testClosePool(t *testing.T, p *WorkerPool) {
@@ -97,68 +149,46 @@ func TestRunSingleJob(t *testing.T) {
 }
 
 // tries to run multiple jobs
-func TestRunMultipleJob(t *testing.T) {
+func TestRunMultipleJobs(t *testing.T) {
+
+	const workerLimit = 4
 
 	// create pool
-	p := NewWorkerPool(4)
+	p := NewWorkerPool(workerLimit)
 	jobQueue := make(chan Job)
 	p.Run(jobQueue)
 
 	// check running pool num workers created
-	if len(p.Workers) != 4 {
+	if len(p.Workers) != workerLimit {
 		t.Error("Wrong number of Workers")
 	}
 
 	// let's create a test job
-	ret := NewJobResultChannel()
-	m := make(map[string]string)
-	x, y := 1, 2
-	m["x"] = strconv.Itoa(x)
-	m["y"] = strconv.Itoa(y)
-	task := NewTestSumTask()
-	work := NewJob(&task, m, ret)
+	job1 := NewSumJobTask(1,2)
 
 	// let's create a test job 2
-	ret2 := NewJobResultChannel()
-	m2 := make(map[string]string)
-	x2, y2 := 3, 4
-	m2["x"] = strconv.Itoa(x2)
-	m2["y"] = strconv.Itoa(y2)
-	task2 := NewTestSumTask()
-	work2 := NewJob(&task2, m2, ret2)
+	job2 := NewSumJobTask(3,4 )
 
 	// let's create a test job 3
-	ret3 := NewJobResultChannel()
-	m3 := make(map[string]string)
-	x3, y3 := 5, 6
-	m3["x"] = strconv.Itoa(x3)
-	m3["y"] = strconv.Itoa(y3)
-	task3 := NewTestSumTask()
-	work3 := NewJob(&task3, m3, ret3)
+	job3 := NewSumJobTask(5,6 )
 
 	// let's create a test job 4
-	ret4 := NewJobResultChannel()
-	m4 := make(map[string]string)
-	x4, y4 := 5, 6
-	m4["x"] = strconv.Itoa(x4)
-	m4["y"] = strconv.Itoa(y4)
-	task4 := NewTestSumTask()
-	work4 := NewJob(&task4, m4, ret4)
+	job4 := NewSumJobTask(7, 8)
 
 	// Push each job onto the queue.
-	jobQueue <- work
-	jobQueue <- work2
-	jobQueue <- work3
-	jobQueue <- work4
+	jobQueue <- job1
+	jobQueue <- job2
+	jobQueue <- job3
+	jobQueue <- job4
 
 	// Consume the merged output from all jobs and check matching sum result
 	sum := int64(0)
-	for n := range Merge(ret, ret2, ret3, ret4) {
+	for n := range Merge(job1.ReturnChannel, job2.ReturnChannel, job3.ReturnChannel, job4.ReturnChannel) {
 		result := n.Value
 		sum += result.(int64)
 	}
 
-	if sum != 32 {
+	if sum != 36 {
 		t.Error("error while summing all results from merged Jobs")
 	}
 
@@ -166,12 +196,10 @@ func TestRunMultipleJob(t *testing.T) {
 	testClosePool(t, &p)
 }
 
-const (
-	workerLimit = 64000
-)
+// tries to run workerLimit = 64000 multiple jobs
+func TestRunThousandMultipleJob(t *testing.T) {
 
-// tries to run massively multiple jobs
-func TestRunMassivelyMultipleJob(t *testing.T) {
+	const workerLimit = 64000
 
 	// create pool
 	p := NewWorkerPool(workerLimit)
@@ -184,34 +212,192 @@ func TestRunMassivelyMultipleJob(t *testing.T) {
 	}
 
 	// let's create the test jobs
-
-	var ret [workerLimit]chan JobResult
+	ret := make([]<-chan JobResult, 0)
 
 	for i := 0; i < workerLimit; i++ {
-		ret[i] = NewJobResultChannel()
-		m := make(map[string]string)
-		x, y := 5, 7
-		m["x"] = strconv.Itoa(x)
-		m["y"] = strconv.Itoa(y)
-		task := NewTestSumTask()
-		work := NewJob(&task, m, ret[i])
+		job := NewSumJobTask(5, 7)
+
 		// Push the job onto the queue.
-		jobQueue <- work
+		jobQueue <- job
+		ret = append(ret, job.ReturnChannel)
 	}
 
 	// Consume the merged output from all jobs and check matching sum result
 	sum := int64(0)
-	var retSlice [workerLimit]<-chan JobResult
-	for i, r := range ret {
-		retSlice[i] = r
-	}
-	for n := range Merge(retSlice[:]...) {
+	for n := range Merge(ret...) {
 		result := n.Value
 		sum += result.(int64)
 	}
 
 	if sum != (12 * workerLimit) {
 		t.Error("error while summing all results from merged Jobs")
+	}
+
+	// try to close pool
+	testClosePool(t, &p)
+}
+
+// tries to run multiple jobs SumRune
+func TestRunMultipleSumRuneJobs(t *testing.T) {
+
+	const workerLimit = 4
+
+	// create pool
+	p := NewWorkerPool(workerLimit)
+	jobQueue := make(chan Job)
+	p.Run(jobQueue)
+
+	// check running pool num workers created
+	if len(p.Workers) != workerLimit {
+		t.Error("Wrong number of Workers")
+	}
+
+	// let's create a test job
+	job1 := NewSumRunesOfWordJobTask("a new era of information is arising with guilhebl software solutions ... 123456789")
+
+	// let's create a test job 2
+	job2 := NewSumRunesOfWordJobTask(" a b c d e f g h i j k l m n o p q r s t u v x z ")
+
+	// let's create a test job 3
+	job3 := NewSumRunesOfWordJobTask(" test data !@#$%^" )
+
+	// let's create a test job 4
+	job4 := NewSumRunesOfWordJobTask(" !@#!%$#%!#%!#%!#%!_#)%*_@$)*%@#+%(#%{PQWIOR{PQIRAF:ALSKFMZX>V M>A< BDKJAS{PQOIRQOPWIRQ QWIEQ{WPI!@_$)(!_)%(*" )
+
+	// Push each job onto the queue.
+	jobQueue <- job1
+	jobQueue <- job2
+	jobQueue <- job3
+	jobQueue <- job4
+
+	// Consume the merged output from all jobs and check matching multiplication value
+	sum := int64(0)
+	for n := range Merge(job1.ReturnChannel, job2.ReturnChannel, job3.ReturnChannel, job4.ReturnChannel) {
+		result := n.Value
+		sum += result.(int64)
+	}
+
+	if sum != 17311 {
+		t.Error("error while summing all results from merged Jobs")
+	}
+
+	// try to close pool
+	testClosePool(t, &p)
+}
+
+// in order to create sample words
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSequenceString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// tries to run workerLimit = 64000 multiple jobs - SumRune
+func TestRunThousandMultipleSumRuneJob(t *testing.T) {
+
+	const workerLimit = 64000
+
+	// create pool
+	p := NewWorkerPool(workerLimit)
+	jobQueue := make(chan Job)
+	p.Run(jobQueue)
+
+	// let's create the test jobs
+	ret := make([]<-chan JobResult, 0)
+
+	for i := 0; i < workerLimit; i++ {
+		job := NewSumRunesOfWordJobTask("ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789")
+
+		// Push the job onto the queue.
+		jobQueue <- job
+		ret = append(ret, job.ReturnChannel)
+	}
+
+	// Consume the merged output from all jobs and check matching sum result
+	sum := int64(0)
+	for n := range Merge(ret...) {
+		result := n.Value
+		sum += result.(int64)
+	}
+
+	if sum != (461760000) {
+		t.Error("error while summing all results from merged SumRune Jobs")
+	}
+
+	// try to close pool
+	testClosePool(t, &p)
+}
+
+// tries to run workerLimit = 100000 multiple jobs - SumRune
+func TestRunHundredThousandMultipleSumRuneJob(t *testing.T) {
+
+	const workerLimit = 100000
+
+	// create pool
+	p := NewWorkerPool(workerLimit)
+	jobQueue := make(chan Job)
+	p.Run(jobQueue)
+
+	// let's create the test jobs
+	ret := make([]<-chan JobResult, 0)
+
+	for i := 0; i < workerLimit; i++ {
+		job := NewSumRunesOfWordJobTask("ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789")
+
+		// Push the job onto the queue.
+		jobQueue <- job
+		ret = append(ret, job.ReturnChannel)
+	}
+
+	// Consume the merged output from all jobs and check matching sum result
+	sum := int64(0)
+	for n := range Merge(ret...) {
+		result := n.Value
+		sum += result.(int64)
+	}
+
+	if sum != (721500000) {
+		t.Error("error while summing all results from merged SumRune Jobs")
+	}
+
+	// try to close pool
+	testClosePool(t, &p)
+}
+
+// tries to run workerLimit = 1000000 multiple jobs - SumRune
+func TestRunMillionMultipleSumRuneJob(t *testing.T) {
+
+	const workerLimit = 1000000
+
+	// create pool
+	p := NewWorkerPool(workerLimit)
+	jobQueue := make(chan Job)
+	p.Run(jobQueue)
+
+	// let's create the test jobs
+	ret := make([]<-chan JobResult, 0)
+
+	for i := 0; i < workerLimit; i++ {
+		job := NewSumRunesOfWordJobTask("ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789 ABCDEFGHIJKLMNOPQRSTUVXYZ123456789")
+
+		// Push the job onto the queue.
+		jobQueue <- job
+		ret = append(ret, job.ReturnChannel)
+	}
+
+	// Consume the merged output from all jobs and check matching sum result
+	sum := int64(0)
+	for n := range Merge(ret...) {
+		result := n.Value
+		sum += result.(int64)
+	}
+
+	if sum != (7215000000) {
+		t.Error("error while summing all results from merged SumRune Jobs")
 	}
 
 	// try to close pool
